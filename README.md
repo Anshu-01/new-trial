ffc2d0459a39f896b559fb2aa4892d6e86cdca34
Anshu-01 <73426618+Anshu-01@users.noreply.github.com>
2021-11-16 18:55:05 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# trex
+# angryBirdsStage2
+Angry Birds stage 2 with Class Inheritance and Images
--- sketch.js
+++ sketch.js
@@ -1,13 +1,31 @@
-function setup() {
-  
-  createCanvas(400, 400);
-  
+var hypnoticBall, database;
+var position;
+
+function setup(){
+createCanvas(500,500);
+database = firebase.database();
+console.log(database);
+ball = createSprite(250,200,40,40);
+ball.shapeColor = "red";
 }
 
-function draw() {
-  background('black');
-  rect(10,160,10,70);
-  rect(380,mouseY,10,70);
-  rect(195,195,10,10);
-  
+function draw(){
+    if(keyDown("left")){
+        changePosition(-1,0);
+    }
+    else if(keyDown("right")){
+        changePosition(1,0);
+    }
+    else if(keyDown("up")){
+        changePosition(0,-1);
+    }
+    else if(keyDown("down")){
+        changePosition(0,1);
+    }
+drawSprites();
+}
+
+function changePosition(x,y){
+ball.x = bal.x + x;
+ball.y = ball.y + y;
 }
